198a45d429ff76d2b32b134c89e8e3c71ff6e508
Владимир <169135025+Mountainskier@users.noreply.github.com>
2024-05-13 06:10:11 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,7 @@
+My_string = input('Введи текст: ')
+print(len(My_string))
+print(My_string.upper())
+print(My_string.lower())
+print(My_string.replace(' ', ''))
+print(My_string[0])
+print((My_string[-1]))
